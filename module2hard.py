5f076f337bf2629e254126010307fca0afc89566
Vladislav <pyny94@mail.ru>
2024-06-02 21:04:19 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -6,9 +6,8 @@ result = ''
 print(x)
 for i in range(1,x):
     for j in range(i+1,x):
-        if x % (int(i) + int(j)) == 0  and i < j:
-            result.append(i)
-            result.append(j)
+        if x % (int(i) + int(j)) == 0 and i < j:
+            result += i + j
 
 print(result)
 
