4a1e18cbbc0968f41bb288823a50eb5fe80a020f
Vladislav <pyny94@mail.ru>
2024-06-02 20:57:18 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,15 @@
+import random
+
+n = list(range(3,20))
+x = random.choice(n)
+result = []
+print(x)
+for i in range(1,x):
+    for j in range(i+1,x):
+        if int(i) != 0 and int(j) != 0 and x % (int(i) + int(j)) == 0 and i != j and i < j:
+            result.append(i)
+            result.append(j)
+
+password = (" ".join(map(str,result)))
+print(password)
+
